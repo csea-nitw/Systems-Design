9ccee0933f950fcfd89107587ce518084dbb20fd
Divas Jindal <divasjindal@gmail.com>
2020-08-22 12:00:32 +0530

    initial commit

--- rate_limiting/database.js
+++
@@ -1,9 +0,0 @@
-const database = {
-    ['index.html']: '<html>Hello Worls</html>',
-};
-
-module.exports.get = (key, callback) => {
-    setTimeout(() =>{
-        callback(database[key]);
-    },1000);
-};
